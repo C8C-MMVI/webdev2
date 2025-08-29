388387ee447ba7e3a2c3701cbdea8fd385f90afd
CCSE Student <ivanandre.santos@lorma.edu>
2025-08-29 13:18:18 +0800

    I'M FUCKING DONE!!!

--- car/src/main/java/com/nav/car/CarController.java
+++ car/src/main/java/com/nav/car/CarController.java
@@ -5,6 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -19,8 +20,8 @@ public class CarController {
 
     @GetMapping("/")
     public String index(Model model){
-        List<com.nav.car.Car> cars = carRepository.findAll();
-        model.addAttribute("cars",carRepository.findAll());
+        List<Car> cars = carRepository.findAll();
+        model.addAttribute("cars", cars);
         cars.forEach(car -> {
             System.out.println(car.getMake());
         });
@@ -29,31 +30,35 @@ public class CarController {
 
     @GetMapping("/add")
     public String add(Model model){
-        com.nav.car.Car car = new com.nav.car.Car();
-        model.addAttribute("car",car);
+        Car car = new Car();
+        model.addAttribute("car", car);
         return "add";
     }
 
     @PostMapping("/save")
-    public String save(@ModelAttribute com.nav.car.Car car){
+    public String save(@ModelAttribute Car car){
         carRepository.save(car);
         return "redirect:/";
     }
 
+    // Fixed: Use @RequestParam to get the car ID from the URL
     @GetMapping("/edit")
-    public String edit(@ModelAttribute com.nav.car.Car car){
-        return "update";
+    public String edit(@RequestParam("id") int id, Model model){
+        Car car = carRepository.findById(id).orElse(new Car());
+        model.addAttribute("car", car);
+        return "edit";
     }
 
     @PostMapping("/update")
-    public String update(@ModelAttribute com.nav.car.Car car){
+    public String update(@ModelAttribute Car car){
         carRepository.save(car);
         return "redirect:/";
     }
 
+    // Fixed: Use @RequestParam to get the car ID from the URL
     @GetMapping("/delete")
-    public String delete(@ModelAttribute com.nav.car.Car car){
-        carRepository.delete(car);
+    public String delete(@RequestParam("id") int id){
+        carRepository.deleteById(id);
         return "redirect:/";
     }
 }
